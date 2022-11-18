0dba6217d4c391ad4d00961b292030a0e6a5ec2b
a-champion <a.champion1506@gmail.com>
2022-11-18 16:04:49 -0700

    created controller and root route

---
+++ controllers/withMe.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const router = express.Router();
+// const data = require('../models/schema.js');
+
+test
+router.get('/', (req, res) => {
+    res.send('Hello WithMe from controller');
+});
+
+module.exports = router;
+
+
--- server.js
+++ server.js
@@ -2,18 +2,30 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const app = express();
-
-
-
-
+const db = mongoose.connection;
+const methodOverride = require('method-override');
+//config
+app.use(methodOverride('_method'));
 let PORT = 3000;
 if(process.env.PORT){
     PORT = process.env.PORT
 }
+app.use(express.urlencoded({extended: true}));
+// app.use(express.json());
+// 
+
+app.use(express.static('public'));
+
+const appController = require ('./controller/withMe.js')
+
 //test
-app.get('/', (req, res) => {
-    res.send('Hello WithMe');
-});
+// app.get('/', (req, res) => {
+//     res.send('Hello WithMe');
+// });
+
+//seed
+
+
 
 app.listen(PORT, () => {
     console.log('listening...');
