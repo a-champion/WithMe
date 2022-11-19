882e4d89708c2fe41c7d0b5fc3e62de5269b1b80
a-champion <a.champion1506@gmail.com>
2022-11-19 09:59:35 -0700

    attempted fix for heroku

--- controllers/withMe.js
+++ controllers/withMe.js
@@ -2,10 +2,24 @@ const express = require('express');
 const router = express.Router();
 // const data = require('../models/schema.js');
 
-// test
+// Index / Home
+//create
 router.get('/', (req, res) => {
-    res.send('Hello WithMe Router');
+    res.render('index.ejs');
 });
+// //read
+// router.get('/', (req, res) => {
+//     res.send('Hello WithMe Router');
+// });
+// //update
+// router.get('/', (req, res) => {
+//     res.send('Hello WithMe Router');
+// });
+// //delete
+// router.get('/', (req, res) => {
+//     res.send('Hello WithMe Router');
+// });
+
 
 module.exports = router;
 
--- server.js
+++ server.js
@@ -4,18 +4,9 @@ const express = require('express');
 const app = express();
 
 // mongoose
-require('dotenv/config');
+// require('dotenv/config');
 const mongoose = require('mongoose');
-// const mongoURI = 'mongodb+srv://vile:Achampionbrah01@project0.4hkgkbk.mongodb.net/collectibles?retryWrites=true&w=majority';
 const db = mongoose.connection;
-
-//controller
-const appController = require ('./controllers/withMe.js');
-
-//method override
-// const methodOverride = require('method-override');
-// app.use(methodOverride('_method'));
-
 
 //config
 let PORT = 3000;
@@ -23,9 +14,26 @@ if(process.env.PORT){
     PORT = process.env.PORT
 }
 
-//body parser
-// app.use(express.urlencoded({extended: true}));
-// app.use(express.json());
+//controller
+const appController = require('./controllers/withMe.js');
+app.use('/WithMe', appController);
+
+//method override
+// const methodOverride = require('method-override');
+// app.use(methodOverride('_method'));
+
+//build path
+// if (process.env.NODE_ENV === 'production') {
+//     app.use(express.static('clients/build'));
+//     app.get('*', (req, res) => {
+//         res.sendFile(path.join(__dirname, 'client', 'build', 'index.html')); //relative path (import module path?)
+
+//     });
+// }
+
+// body parser
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
 
 //static
 // app.use(express.static('public'));
@@ -38,12 +46,11 @@ if(process.env.PORT){
 //seed
 
 
-
 app.listen(PORT, () => {
     console.log('listening...');
 });
 
-mongoose.connect(process.env.DB_CONNECTION, () => {
+mongoose.connect(process.env.DB_CONNECTION || 'mongodb://127.0.0.1:27017/WithMe', () => {
     console.log('connected to mongo atlas');
 });
 
