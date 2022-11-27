b77712afe29ac9c39a91fb8114639378e2f1b8b4
a-champion <a.champion1506@gmail.com>
2022-11-27 16:51:31 -0700

    users/sessions functioning - creating edit/show pages rousers and implementing custom collections begins

--- controllers/users.js
+++ controllers/users.js
@@ -9,12 +9,38 @@ users.get('/new', (req, res) => {
     });
 });
 
+users.get('/', (req, res) => {
+    if (req.session.currentUser) {
+        User.findById(req.params.id, (err, foundUser) => {
+            res.render('users/show.ejs', {
+                user: foundUser,
+                currentUser: req.session.currentUser
+            });
+        });
+    } else {
+        res.redirect('/sessions/new');
+    }
+});
+
+users.get('/:id/edit', (req, res) => {
+    if (req.session.currentUser) {
+        User.findById(req.params.id, (err, foundUser) => {
+            res.render('users/edit.ejs', {
+                user: foundUser,
+                currentUser: req.session.currentUser
+            });
+        });
+    } else {
+        res.redirect('/sessions/new');
+    }
+});
+
 users.post('/', (req, res) => {
     console.log(req.body);
     req.body.password = bcrypt.hashSync(req.body.password, bcrypt.genSaltSync(10));
     User.create(req.body, (err, createdUser) => {
         console.log('user is created', createdUser);
-        res.redirect('/users');
+        res.redirect('/sessions/new');
     })
 })
 
--- models/users.js
+++ models/users.js
@@ -1,13 +1,18 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
-// const Collectable = require('./collectables.js');
+const Collectable = require('./collectables.js');
 
 const userSchema = new Schema({
     username:{ type: String, unique: true, required: true },
-    password:{ type: String, required: true }
-    // ,
-    // collections:[Collectable.schema]
+    password:{ type: String, required: true },
+    name:{ type: String, required: false, maxlength: 32, trim: true },
+    lname: { type: String, required: false, maxlength: 32, trim: true },
+    birthday:{ type: Date, required: false},
+    email:{ type:String, unique:true, required: false, trim: true},
+    mobile: { type: String, required: false},
+    interests:{type: [ String ], required: false},
+    collections:{type: [Collectable.schema], required: false}
 }, {timestamps:true});
 
 const User = mongoose.model('User', userSchema);
-module.exports = User
+module.exports = User;
